04f7593f148972958fb18b97943f1fff7f909108
opiyodhiambo <rnldlinus@gmail.com>
2023-08-20 11:31:37 +0300

    Refactored the code

--- src/main/scala/adventure/Getter.scala
+++ src/main/scala/adventure/Getter.scala
@@ -1,9 +1,17 @@
-import org.asynchttpclient.AsyncHttpClient
+  package adventure
 
-val client = new AsyncHttpClient 
-def get (url: String): String = {
-  val response = client.prepareGet(url).execute().get
-  if (response.getStatusCode < 400)
-    response.getResponseBodyExcerpt(131072)
-  else throw BadStatus(response.getStatusCode)
+  private val client = new AsyncHttpClient 
+  def get (url: String)(implicit exec: Executor): Future[String] = {
+    val f = client.prepareGet(url).execute();
+    val p = Promise[String]()
+    f.addListener(new Runnable {
+      def run = {
+        val response = f.get
+        if (response.getStatusCode < 400)
+          p.success(response.getResponseBodyExcerpt(131072))
+          else p.failure(BadStatus(response.getStatusCode))
+      }
+    }, exec)
+    p.future
+
 }
