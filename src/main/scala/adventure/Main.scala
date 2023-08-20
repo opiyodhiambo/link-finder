4e54e88f171c99e43097577166182bb70318fbe8
opiyodhiambo <rnldlinus@gmail.com>
2023-08-20 15:49:23 +0300

    defined the post stop method that shuts down the webclient

--- src/main/scala/adventure/Main.scala
+++ src/main/scala/adventure/Main.scala
@@ -6,6 +6,13 @@ import akka.actor.ReceiveTimeout
 class Main extends Actor {
   val receptionist = context.actorOf(Props[Receptionist], "receptionist")
   receptionist ! Get("http://www.google.com")
+  receptionist ! Get("http://www.google.com/1")
+  receptionist ! Get("http://www.google.com/2")
+  receptionist ! Get("http://www.google.com/3")
+  receptionist ! Get("http://www.google.com/4")
+
+
+
   context.setReceiveTimeout(10.seconds)
   def receive: Actor.Receive = {
     case Result(url, set) =>
@@ -14,5 +21,8 @@ class Main extends Actor {
       println(s"Failed to fetch '$url'\n")
     case ReceiveTimeout =>
       context.stop(self)
+  }
+  override def postStop(): Unit = {
+    WebClient.shutdown()
   }
 }
