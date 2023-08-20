5a202b626b23632326d0135a654510e42fd01b7b
opiyodhiambo <rnldlinus@gmail.com>
2023-08-20 12:48:34 +0300

    defined receive and stop methods

--- src/main/scala/adventure/Getter.scala
+++ src/main/scala/adventure/Getter.scala
@@ -1,8 +1,30 @@
-class Getter(url: String, depth: Int) extends Actor {
+package adventure
+
+import akka.actor.Actor
+import adventure.*
+import akka.actor.Status
+
+class Getter(url: String, depth: Int) extends Actor { // takes the url to visit and the depth of the link
   implicit val exec = context.dispatcher
-  val future = WebClient.get(url)
-  future onComplete {
-    case Success(body) => self ! body 
-    case Failure(err) => self ! Status.Failure(err)
+  val future = WebClient.get(
+    url
+  ) // Fetch the link using the WebClient and gives us back a future
+  WebClient get url pipeTo self // send message to the actor when future is complete
+
+  def receive = {
+    case body: String =>
+      for (
+        link <- findLinks(body)
+      ) // get the links using the findLinks method in the WebClient class
+        context.parent ! Controller.Check(
+          link,
+          depth
+        ) // Check the links and send the message to the parent actor
+      stop() // The actor then stops, a method defined below
+    case _: Status.Failure => stop()
+  }
+  def stop(): Unit = {
+    context.parent ! Done
+    context.stop(self)
   }
 }
