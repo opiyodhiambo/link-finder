649a574b5e069ed66c9b6ea1bcde016c4b7e21cf
opiyodhiambo <rnldlinus@gmail.com>
2023-08-20 15:14:03 +0300

    Executed the main functions using the helper functions

--- src/main/scala/adventure/Receptionist.scala
+++ src/main/scala/adventure/Receptionist.scala
@@ -5,22 +5,35 @@ import akka.actor.Actor
 import akka.actor.Props
 class Receptionist extends Actor {
   def receive: Actor.Receive = waiting
-  val waiting: Receive = {
-    // Upon Get (url) start a traversal and become running
+  val waiting: Receive = { case Get(url) =>
+    context.become(runNext(Vector(Job(sender, url))))
   }
   def running(queue: Vector[Job]): Receive = {
-    //upon Get(url) append that to queue and keep running
-    //Upon Controller.Results, shot ot tp the client and run that to the next job from queue if any
+    // upon Get(url) append that to queue and keep running
+    case Get(url) =>
+      context.become(enqueueJob(queue, Job(sender, url)))
+   // Upon Controller.Results, shot ot tp the client and run that to the next job from queue if any 
+    case Controller.Result(links) =>
+      val job = queue.head 
+      job.client ! Result(job.url, links)
+      context.stop(sender)
+      context.become(runNext(queue.tail))
   }
   case class Job(client: ActorRef, url: String)
   var reqNo = 0
   def runNext(queue: Vector[Job]): Receive = {
-    reqNo += 1 
-    if (queue.isEmpty) waiting
+    reqNo += 1
+    if (queue.isEmpty) waiting // when the job queue is empty, the receptionist waits
     else {
-      val controller = context.actorOf(Props[Controller], s"c$reqNo")
-      controller ! Controller.Check(queue.head.url, 2)
-      running(queue)
+      val controller = context.actorOf(Props[Controller], s"c$reqNo") // Otherwise, we instantiate a new controller
+      controller ! Controller.Check(queue.head.url, 2) // then delegate the task, which is to check the link
+      running(queue) // then we transition into the running state
     }
+  }
+  def enqueueJob(queue: Vector[Job], job: Job): Receive = {
+    if (queue.size > 3) {
+      sender ! Failed(job.url)
+      running(queue)
+    } else running(queue :+ job)
   }
 }
