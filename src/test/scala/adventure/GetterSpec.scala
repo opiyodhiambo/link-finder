380b59cb14c08e3bd63394290928cfd84c3018d4
opiyodhiambo <rnldlinus@gmail.com>
2023-08-20 17:43:47 +0300

    created a fake webclient

---
+++ src/test/scala/adventure/GetterSpec.scala
@@ -0,0 +1,21 @@
+val firstLink = "http://www.adventure.info/1"
+
+val bodies = Map(
+  firstLink ->
+    """"<html>
+          <head><title>Page 1 </title></head>
+          <body>
+            <h1>A link</h1>
+            <a href="http://adventure.info/2">click here</a>
+          </body>
+      </html>""".stripMargin
+)
+
+val links = Map(firstLink -> Seq("http://adventure.info/2"))
+object FakeWebClient extends WebClient {
+  def get(url: String)(implicit exec: Executor): Future[String] = 
+    bodies get url martch {
+      case None => Future.failed(BadStatus(404))
+      case Some(body) => Future.successful(body)
+    }
+}
