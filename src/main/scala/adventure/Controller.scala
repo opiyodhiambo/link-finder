c6a0334e07c3b7c5b175bd71600490cc9ecb943f
opiyodhiambo <rnldlinus@gmail.com>
2023-08-20 13:11:45 +0300

    Enabled the actor to check the url, cache the results and end the process

--- src/main/scala/adventure/Controller.scala
+++ src/main/scala/adventure/Controller.scala
@@ -1,11 +1,18 @@
 package adventure
 import adventure.*
+import akka.actor.{Actor, ActorLogging, ActorRef}
 
 class Controller extends Actor with ActorLogging {
-  var cache = Set.empty[String]
-  var children = Set.empty[String]
+  var cache = Set.empty[String] // holds the cached results of the visited strings
+  var children = Set.empty[ActorRef] //keeps track of all the child actors created 
   def receive = {
-    case Check => ???
-    case Getter.Done => ???
+    case Check(url, depth) =>
+      log.debug("{} checking {}", depth, url) // Log every check request at debug level
+      if (!cache(url) && depth > 0) // if the cache already has the url, or has a max depth of zero, nothing is done
+        children += context.actorOf(Props(new Getter(url, depth -1))) // otherwise, create a new getter  and tell it about the url to fetch, decreasing the depth by 1
+      cache += url // append the visited url to the cache 
+    case Getter.Done => 
+      children -= sender // once the getter is done, we remove it from the children set 
+      if (children.isEmpty) context.parent ! Result(cache) // Once no getter is there anymore, we know that the whole process is fin ished an we tell the parent the results
   }
 }
